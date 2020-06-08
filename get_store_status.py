b6d3efbbba97d9d6ab241819d7a8ddf05febfdd4
Pavlo Zhoholiev <zhogolevpv@gmail.com>
2020-06-08 14:03:13 +0300

    Добавил магазин Рандуву

--- get_store_status.py
+++ get_store_status.py
@@ -1,8 +1,11 @@
 from webapp_stores import create_app
 from webapp_stores.store_butik_ru import Butik_ru
+from webapp_stores.srote_rendezvous import Rendezvous
 
 app = create_app()
 with app.app_context():
      butik = Butik_ru()
      butik.get_butik_data()
+     randezvous = Rendezvous()
+     randezvous.get_butik_data()
 
---
+++ webapp_stores/srote_rendezvous.py
@@ -0,0 +1,61 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+from webapp_stores.model import db, Model_stores
+
+
+class Rendezvous():
+
+    def __init__(self, url='https://www.rendez-vous.ru/'):
+        self.url = url
+
+    def standard_icon(self):
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        standard_icon_path =  basedir +'\\images\\'+'smile.png'
+        return standard_icon_path
+
+    def get_rendezvous(self):
+        try:
+            result = requests.get(self.url)
+            result.raise_for_status()
+            return result.text
+        except(requests.RequestException, ValueError):
+            return False
+
+    def get_butik_data(self):
+        html = self.get_rendezvous()
+        if html:
+            soup = BeautifulSoup(html, 'html.parser')
+            store_title = soup.title.string
+            store_name = 'Rendezvous'
+            store_icon = soup.find('meta',property="og:image",)['content']
+            store_online = True
+            store_url = self.url
+            self.save_data(store_title=store_title, store_online=store_online, store_url=store_url,
+                           store_name = store_name, store_icon = store_icon )
+        else:
+            store_online = False
+            store_url = self.url
+            store_name = 'Rendezvous'
+            store_title = 'Магазин временно недоступен'
+            store_icon = self.standard_icon()
+            self.save_data(store_title=store_title, store_online=store_online, store_url=store_url,
+                           store_name = store_name,store_icon = store_icon)
+
+    def save_data(self, store_title, store_online, store_url,store_name,store_icon):
+        if Model_stores.query.filter(Model_stores.store_url == store_url,
+                                          Model_stores.store_name == store_name).count():
+            Model_stores.query.filter(Model_stores.store_online != store_online).update({'store_online': store_online })
+            Model_stores.query.filter(Model_stores.store_title != store_title).update({'store_title': store_title})
+            Model_stores.query.filter(Model_stores.store_icon != store_icon ).update({'store_icon': store_icon})
+            db.session.commit()
+        else:
+            base_update = Model_stores(store_title=store_title, store_online=store_online, store_url=store_url,
+                                       store_name = store_name, store_icon = store_icon)
+            db.session.add(base_update)
+            db.session.commit()
+
+
+if __name__ == '__main__':
+    c = Rendezvous()
+    print(c.get_butik_data())
