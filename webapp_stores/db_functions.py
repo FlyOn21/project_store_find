78bf54132e53824e796c099ac5c1a66c8d1b02ca
Zhoholiev Pavlo <zhogolevpv@gmail.com>
2020-06-27 21:39:46 +0300

    мердж веток 27_06_2020_2

--- webapp_stores/db_functions.py
+++ webapp_stores/db_functions.py
@@ -4,26 +4,26 @@ from webapp_stores.model import db, Stores, Product, InterestingProduct
 def save_data(title, online, url, name, icon):
     if Stores.query.filter(Stores.url == url, Stores.name == name).count() \
             and Stores.query.filter(Stores.online != online):
-        store = Stores.query.filter(Stores.url == url, Stores.name == name).first()
+        store = Stores.query.filter(Stores.url == url,Stores.name == name).first()
         store.online = online
         store.title = title
         store.icon = icon
         db.session.add(store)
         db.session.commit()
     else:
-        base_update = Stores(title=title,
+        stores = Stores(title=title,
                              online=online,
                              url=url,
                              name=name,
                              icon=icon)
-        db.session.add(base_update)
+        db.session.add(stores)
         db.session.commit()
 
 
-# Сохранение данных в общую базу данных
 def save_data_product(product_dict):
-    if Product.query.filter(Product.id_store == product_dict['id']).count():
-        product_b = Product.query.filter(Product.id_store == product_dict['id']).first()
+
+    if Product.query.filter(Product.id_product == product_dict['id']).count():
+        product_b = Product.query.filter(Product.id_product == product_dict['id']).first()
         product_b.prise_full = product_dict['price']
         product_b.prise_discount = product_dict['product_discount']
         product_b.color = product_dict['color']
@@ -34,8 +34,14 @@ def save_data_product(product_dict):
         db.session.add(product_b)
         db.session.commit()
     else:
-        add_product_1 = Product(id_store=product_dict['id'],
-                                store=product_dict['product_store'],
+        if product_dict['product_store'] == 'Aliexpress':
+            store = 1
+        elif product_dict['product_store'] == 'Butik.ru':
+            store = 3
+        elif product_dict['product_store'] == "Randevu":
+            store = 2
+        add_product_1 = Product(id_product=product_dict['id'],
+                                store=store,
                                 name=product_dict['name'],
                                 prise_full=product_dict['price'],
                                 prise_discount=product_dict['product_discount'],
@@ -49,9 +55,9 @@ def save_data_product(product_dict):
                                 delivery=(product_dict['delivery'] if 'delivery' in product_dict else None),
                                 other=(product_dict['other'] if 'other' in product_dict else None),
                                 gender=product_dict['gender'])
-
         db.session.add(add_product_1)
         db.session.commit()
+
 
 
 # Сохранение данных в клиентскую базу данных
