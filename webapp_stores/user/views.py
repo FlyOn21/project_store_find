b8d1773bfd0ce6c5498d67959460acf9e8f221fa
pasha <zhogolevpv@gmail.com>
2020-07-16 08:43:53 +0300

    фикс бага счетчика страниц ver 2.3

--- webapp_stores/user/views.py
+++ webapp_stores/user/views.py
@@ -231,6 +231,7 @@ def delete_product():
 
 @blueprint.route("/search", methods=['GET', 'POST'])
 def search():
+
     try:
         search = request.form['search']
         with current_app.app_context():
