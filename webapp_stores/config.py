8e973d9f8ed33839452b7d989df96a27c4c1f3cd
pasha <zhogolevpv@gmail.com>
2020-07-17 17:57:09 +0300

    фикс email  3

--- webapp_stores/config.py
+++ webapp_stores/config.py
@@ -12,12 +12,18 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "k;lkl;wkermw;emr.,wmerl;k;lm/.ml;';l';lm/.m,;lk;lk;lkm,m"
 
 # email server
-MAIL_SERVER = 'smtp.sendgrid.net'
+MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_USERNAME = 'apikey'
-MAIL_PASSWORD = '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'
+MAIL_USERNAME = 'zhogolevpv@gmail.com'
+MAIL_PASSWORD = 'Gfdtk2105'
+# MAIL_SERVER = 'smtp.sendgrid.net'
+# MAIL_PORT = 465
+# MAIL_USE_TLS = False
+# MAIL_USE_SSL = True
+# MAIL_USERNAME = 'apikey'
+# MAIL_PASSWORD = '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'
 
 # administrator list
 ADMINS = ['zhogolevpv@gmail.com']
